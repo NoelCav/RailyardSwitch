74c303e2936891b160a6e2353ab4b51f831b2f92
NoelCav <noelcaverly@msn.com>
2021-03-14 22:47:28 -0400

    Basic IO

--- IO.py
+++ IO.py
@@ -1,12 +1,50 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+#Constants
+FORWARD__PIN = 11
+BACKWARD_PIN = 12
 
-GPIO.setup(18, GPIO.OUT)
-GPIO.output(18, GPIO.HIGH)
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(FORWARD__PIN, GPIO.OUT)
+GPIO.setup(BACKWARD_PIN, GPIO.OUT)
+
+Forward()
 
 time.sleep(3)
 
-GPIO.output(18, GPIO.LOW)
+Backward()
+
+time.sleep(3)
+
+Stop()
+
 GPIO.cleanup()
+
+
+#IO Functions
+def setFWPin(state):
+    if state:
+        GPIO.output(FORWARD__PIN, GPIO.HIGH)
+    else:
+        GPIO.output(FORWARD__PIN, GPIO.LOW)
+
+def setBWPin(state):
+    if state:
+        GPIO.output(BACKWARD_PIN, GPIO.HIGH)
+    else:
+        GPIO.output(BACKWARD_PIN, GPIO.LOW)
+
+def Forward():
+    setBWPin(0)
+    setFWPin(1)
+
+def Backward():
+    setFWPin(0)
+    setBWPin(1)
+
+def Stop():
+    setFWPin(0)
+    setBWPin(0)
