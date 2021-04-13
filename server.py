7db040e35d6ef68c25fb857aea04900bc8d374e4
Hima <himapatelk2016@gmail.com>
2021-04-12 20:44:01 -0700

    workinggetrequest

---
+++ server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import argparse
+from http.server import HTTPServer, BaseHTTPRequestHandler
+#from IO.py import switchaction
+
+#add below function to io.py
+# def switchaction(message):
+#     if(message == "/open"):
+#         switch.forward()
+#         print ("open")
+#     elif(message == "/close"):
+#         switch.backward()
+#         print ("close")
+#     else:
+#         print("Invalid action")
+
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def _html(self, message):
+        #here
+        switchaction(message)
+        
+        content = f"This is message {message}>"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(self._html(self.path))
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write(self._html("POST!"))
+
+
+def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
+    parser.add_argument(
+        "-l",
+        "--listen",
+        default="localhost",
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="Specify the port on which the server listens",
+    )
+    args = parser.parse_args()
+    run(addr=args.listen, port=args.port)
